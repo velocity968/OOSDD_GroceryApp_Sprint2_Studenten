958d4116f1322dfbde7617591869c8a824cbdb46
velocity968 <velo45145@gmail.com>
2025-09-18 15:28:42 +0200

    Added loginScreen
    Added fully functional loginscreen

--- Grocery.Core.Data/Repositories/ClientRepository.cs
+++ Grocery.Core.Data/Repositories/ClientRepository.cs
@@ -19,9 +19,15 @@ namespace Grocery.Core.Data.Repositories
 
         public Client? Get(string email)
         {
-            return clientList[0];
+            foreach (var client in clientList)
+            {
+                if (client.EmailAddress == email)
+                    return client;
+            }
+            return null;
         }
 
+         
         public Client? Get(int id)
         {
             return clientList[0];
